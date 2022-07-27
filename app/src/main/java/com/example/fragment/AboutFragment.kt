9dc322f9675dd0e9b8901f988a109c5ecf59dafc
anreh <anurehka@gmail.com>
2022-07-27 11:05:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragment/AboutFragment.kt
@@ -0,0 +1,34 @@
+package com.example.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.fragment.databinding.FragmentAboutBinding
+import com.example.fragment.databinding.FragmentHomeBinding
+
+class AboutFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_about, container, false)
+
+        binding.btnHome.setOnClickListener(){
+            Navigation.findNavController(binding.root)
+                .navigate(R.id.action_aboutFragment_to_homeFragment)
+        }
+
+        return binding.root
+    }
+
+
+}
